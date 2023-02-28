01c955ea11beda3169ab0e51ed77713b49a81860
Mirza Hilmi <118604764+MirzaHilmi@users.noreply.github.com>
2023-02-28 11:34:17 +0700

    modify method challenge output

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,20 +5,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Methods myMethod = new Methods();
         String[] names = new String[5];
+        int[] scores = new int[5];
 
         for (int i = 0; i < names.length; i++) {
             names[i] = sc.nextLine();
+            scores[i] = sc.nextInt();
+            sc.nextLine();
         }
         sc.close();
 
-        myMethod.printName(names);
+        myMethod.printName(names, scores);
     }
 }
 
 class Methods {
-    void printName (String[] names) {
-        for (String name : names) {
-            System.out.print(name + " ");
+    void printName (String[] names, int[] scores) {
+        for (int i = 0; i < scores.length; i++) {
+            System.out.printf("%s\t: %d\n", names[i], scores[i]);
         }
     }
 }
