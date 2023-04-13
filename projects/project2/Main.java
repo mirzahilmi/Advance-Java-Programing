4035f5dadc8a689f15d6d1c0fc3b4d30abd549a2
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-13 23:27:45 +0700

    Modify Project 2 to use Scanner

--- projects/project2/Main.java
+++ projects/project2/Main.java
@@ -1,23 +1,45 @@
 package projects.project2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        CarRider ming = new CarRider("Lin Ming", 19, "08000000000");
-        CarRider youhan = new CarRider("Bei Youhan", 20, "0811111111111");
-        CarRider ling = new CarRider("Ling'er", 31, "082222222222");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan nama\t\t: ");
+        String name = sc.nextLine();
+
+        System.out.print("Masukkan umur\t\t: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Masukkan nomor telfon\t: ");
+        String phoneNumber = sc.nextLine();
+
+        CarRider rider = new CarRider(name, age, phoneNumber);
 
         CarData data = new CarData();
-        data.addCar("SUV", "N 1111 AB", "Honda");
-        data.addCar("SPORT", "N 2222 AB", "SSC Tuatara");
-        data.addCar("TRUCK", "N 3333 AB", "Suzuki");
+
+        System.out.println();
+        System.out.print("Tipe Mobil\t: ");
+        String type = sc.nextLine();
+
+        System.out.print("Nomor Plat\t: ");
+        String licensePlate = sc.nextLine();
+
+        System.out.print("Merk Mobil\t: ");
+        String brand = sc.nextLine();
+
+        data.addCar(type, licensePlate, brand);
 
         data.listOfCar();
-        System.out.println("\n");
+
+        System.out.print("\n\nBerapa lama durasi peminjaman? ");
+        int duration = sc.nextInt();
+        sc.close();
 
         RentArchive arsip = new RentArchive();
-        arsip.rent(ming, data.carList.get(1), 9);
-        arsip.rent(youhan, data.carList.get(0), 3);
-        arsip.rent(ling, data.carList.get(1), 1);
+        arsip.rent(rider, data.carList.get(0), duration);
 
         arsip.info();
     }
